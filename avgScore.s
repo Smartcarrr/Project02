a520cba55417b9bdfc943a9f635a51d562c60b76
Smartcarrr <162828603+Smartcarrr@users.noreply.github.com>
2024-12-04 15:07:55 -0800

    Add files via upload

---
+++ avgScore.s
@@ -0,0 +1,142 @@
+.data 
+
+orig: .space 100	# In terms of bytes (25 elements * 4 bytes each)
+sorted: .space 100
+
+str0: .asciiz "Enter the number of assignments (between 1 and 25): "
+str1: .asciiz "Enter score: "
+str2: .asciiz "Original scores: "
+str3: .asciiz "Sorted scores (in descending order): "
+str4: .asciiz "Enter the number of (lowest) scores to drop: "
+str5: .asciiz "Average (rounded down) with dropped scores removed: "
+
+
+.text 
+
+# This is the main program.
+# It first asks user to enter the number of assignments.
+# It then asks user to input the scores, one at a time.
+# It then calls selSort to perform selection sort.
+# It then calls printArray twice to print out contents of the original and sorted scores.
+# It then asks user to enter the number of (lowest) scores to drop.
+# It then calls calcSum on the sorted array with the adjusted length (to account for dropped scores).
+# It then prints out average score with the specified number of (lowest) scores dropped from the calculation.
+main: 
+	addi $sp, $sp -4
+	sw $ra, 0($sp)
+	li $v0, 4 
+	la $a0, str0 
+	syscall 
+	li $v0, 5	# Read the number of scores from user
+	syscall
+	move $s0, $v0	# $s0 = numScores
+	move $t0, $0
+	la $s1, orig	# $s1 = orig
+	la $s2, sorted	# $s2 = sorted
+loop_in:
+	li $v0, 4 
+	la $a0, str1 
+	syscall 
+	sll $t1, $t0, 2
+	add $t1, $t1, $s1
+	li $v0, 5	# Read elements from user
+	syscall
+	sw $v0, 0($t1)
+	addi $t0, $t0, 1
+	bne $t0, $s0, loop_in
+	
+	move $a0, $s0
+	jal selSort	# Call selSort to perform selection sort in original array
+	
+	li $v0, 4 
+	la $a0, str2 
+	syscall
+	move $a0, $s1	# More efficient than la $a0, orig
+	move $a1, $s0
+	jal printArray	# Print original scores
+	li $v0, 4 
+	la $a0, str3 
+	syscall 
+	move $a0, $s2	# More efficient than la $a0, sorted
+	jal printArray	# Print sorted scores
+	
+	li $v0, 4 
+	la $a0, str4 
+	syscall 
+	li $v0, 5	# Read the number of (lowest) scores to drop
+	syscall
+	move $a1, $v0
+	sub $a1, $s0, $a1	# numScores - drop
+	move $a0, $s1
+	jal calcSum	# Call calcSum to RECURSIVELY compute the sum of scores that are not dropped
+	
+	# Your code here to compute average and print it
+	
+	lw $ra, 0($sp)
+	addi $sp, $sp 4
+	li $v0, 10 
+	syscall
+	
+	
+# printList takes in an array and its size as arguments. 
+# It prints all the elements in one line with a newline at the end.
+printArray:
+	# Your implementation of printList here	
+	move $t0, $zero
+	move $t1, $zero
+	move $t2, $a0
+printStart:
+	beq $t0, $a1, printEnd
+	sll $t1, $t0, 2
+	addu $t1, $t1, $t2
+	lw $a0, 0($t1)
+	li $v0, 1
+	syscall
+	li $a0, 32
+	li $v0, 11
+	syscall
+	addiu $t0, $t0, 1
+	j printStart
+printEnd:
+	li $a0, 10
+	li $v0, 11
+	syscall
+	jr $ra
+
+
+	
+# selSort takes in the number of scores as argument. 
+# It performs SELECTION sort in descending order and populates the sorted array
+selSort:
+	# Your implementation of selSort here
+
+	jr $ra
+	
+	
+# calcSum takes in an array and its size as arguments.
+# It RECURSIVELY computes and returns the sum of elements in the array.
+# Note: you MUST NOT use iterative approach in this function.
+calcSum:
+	# Your implementation of calcSum here
+	move $t0, $zero
+	move $t1, $zero
+	move $t2, $a0
+	addi $sp, $sp, -4
+sumStart:
+	beq $t0, $a1, sumEnd
+	sll $t1, $t0, 2
+	addu $t1, $t1, $t2
+	lw $v0, 0($t1)
+	sw $v0, 0($sp)
+	addi $t0, $t0, 1
+	j sumStart
+	lw $t0, 0($sp)
+	add $v0, $v0, $t0
+	
+sumEnd:
+	addi $sp, $sp, 4
+	add $a0, $zero, $v0
+	li $v0, 1
+	syscall
+	jr $ra
+	
